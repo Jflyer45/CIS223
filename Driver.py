2a1c1cc8b62fac2bf53d7236e6a050cd827cd19b
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-01-21 17:56:09 -0600

    Solved Problem 1

--- Driver.py
+++ Driver.py
@@ -1,2 +1,22 @@
 # Writen By: Jeremy Fischer, , and
 from Problem1 import TwoStack
+
+testCase = TwoStack(2, 3)
+
+testCase.Push1(1)
+testCase.Push1(2)
+
+testCase.Push2(5)
+testCase.Push2(4)
+testCase.Push2(4)
+testCase.Push2(466)
+testCase.Push2(13241234)
+print(testCase.head2)
+
+print(testCase.array)
+
+print(testCase.isFull1())
+print(testCase.isFull2())
+
+
+
--- Problem1.py
+++ Problem1.py
@@ -4,24 +4,70 @@
 class TwoStack():
     def __init__(self, size1, size2):
         # Variables
-        list = [None] * (size1 + size2)
+        self.size1 = size1
+        self.size2 = size2
+        self.array = [None] * (size1 + size2)
+        self.head1 = 0
+        self.head2 = len(self.array)-1
 
     def Push1(self, key):
+        # If the final slot is not taken, fill it in with data
+        if(self.array[self.size1-1] is None):
+            self.array[self.head1] = key
+            if(self.head1 != self.size1-1):
+                self.head1 += 1
 
     def Push2(self, key):
+        # If the final slot is not taken, fill it in with data
+        # print()
+        if(self.array[len(self.array) - self.size2] is None):
+            print("this happend")
+            self.array[self.head2] = key
+            if(self.head2 != len(self.array) - self.size2):
+                self.head2 -= 1
 
     def Pop1(self):
+        # TODO what if pop at index 1 where it is null
+        poppedValue = self.array[self.head1]
+        self.array[self.head1] = None
+        if(self.head1 != 0):
+            self.head1 -= 1
+        return poppedValue
 
     def Pop2(self):
+        poppedValue = self.array[self.head2]
+        self.array[self.head2] = None
+        if (self.head2 != len(self.array) - 1):
+            self.head2 += 1
+        return poppedValue
 
     def Peek1(self):
+        return self.array[self.head1]
 
     def Peek2(self):
+        return self.array[self.head2]
 
     def isFull1(self):
+        if(self.array[self.size1 - 1] is None):
+            return False
+        else:
+            return True
 
     def isFull2(self):
+        if (self.array[len(self.array) - self.size2] is None):
+            return False
+        else:
+            return True
 
     def isEmpty1(self):
+        if(self.head1 == 0 and self.array[0] is None):
+            return True
+        else:
+            return False
 
     def isEmpty2(self):
+        topIndex = len(self.array)-1
+        if (self.head2 == topIndex and self.array[topIndex] is None):
+            return True
+        else:
+            return False
