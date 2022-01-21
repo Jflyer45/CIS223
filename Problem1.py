da4fbdbe21547b05882cab707c2dbdff9a8f2695
natebursch <92119395+natebursch@users.noreply.github.com>
2022-01-21 16:25:44 -0600

    Update Problem1.py
    new push and empty

--- Problem1.py
+++ Problem1.py
@@ -17,16 +17,19 @@ class TWOSTACK:
     #methods
     def Push1(self,key):
         #check to see if the list is full
-        if(self.isFull1()== False): #checking to see if the list is full before adding to it
+        if(self.isFull1()): #checking to see if the list is full before adding to it
+            print("Push 1: Not Allowed")
+        else:
             self.stacks.insert(self.pointer,key)
             self.pointer += 1
-        else:
-            print("Not Allowed")
             #because we are pushing to stack 1 we increse the pointer. by inserting, the list automatically pushes stack two
 
     def Push2(self,key):
-        self.stacks.insert(self.pointer,key)
-        #we do not need to change the pointer because value was added to stack two
+        if(self.isFull2()): #checking to see if the list is full before adding to it
+            print("Push 2: Not Allowed")
+        else:
+            self.stacks.insert(self.pointer,key)
+            #we do not need to change the pointer because value was added to stack two
 
 
     def Pop1(self):
@@ -37,6 +40,11 @@ class TWOSTACK:
     def Pop1(self):
         self.stacks.pop()
 
+    def Peek1(self):
+        return True
+    def Peek2(self):
+        return False
+
     def isFull1(self):
         #list one is full if the pointer is  > maxsize
         if(self.pointer >= self.max1):
@@ -45,9 +53,9 @@ class TWOSTACK:
         
 
     def isFull2(self):
-        if(len(self.stacks)== self.size1):
+        if(len(self.stacks)-self.pointer >= self.max2):
             return True
-        else: False
+        else: return False
 
     def isEmpty1(self):
         if(len(self.stacks)==0):
@@ -68,9 +76,9 @@ maxStack2 = butt.max2
 butt.Push1(0)
 butt.Push1(1)
 butt.Push1(2)
-butt.Push1(2)
-butt.Push1(2)
-butt.Push1(2)
+butt.Push1(3)
+butt.Push1(4)
+butt.Push1(5) #not allowed
 
 
 
@@ -81,13 +89,22 @@ butt.Push1(2)
 
 
 
-butt.Push2(3)
-butt.Push2(4)
-butt.Push2(5)
+butt.Push2(6)
+butt.Push2(7)
+butt.Push2(8)
+butt.Push2(9)
+butt.Push2(10)
+butt.Push2(11)#not allowed
 
 
+
+
+length_ofstack = len(butt.stacks)
+
+stack2_length = len(butt.stacks)-butt.pointer
 
 stack1full = butt.isFull1()
+stack2full = butt.isFull2()
 
 print(butt.stacks)
 
