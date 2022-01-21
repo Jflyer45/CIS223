431239fce6ecc640e632e85b56d46b3f80d797ee
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-01-21 14:15:20 -0600

    Set up problem 1 methods

--- Problem1.py
+++ Problem1.py
@@ -1,6 +1,27 @@
 # Writen By: Jeremy Fischer, , and
 
+
 class TwoStack():
-    def __init__(self):
+    def __init__(self, size1, size2):
         # Variables
-        test = 0
+        list = [None] * (size1 + size2)
+
+    def Push1(self, key):
+
+    def Push2(self, key):
+
+    def Pop1(self):
+
+    def Pop2(self):
+
+    def Peek1(self):
+
+    def Peek2(self):
+
+    def isFull1(self):
+
+    def isFull2(self):
+
+    def isEmpty1(self):
+
+    def isEmpty2(self):
