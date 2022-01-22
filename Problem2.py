902f42cabc178cd8d9f304cd2d98a8ed5ebb51d1
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-01-21 18:09:42 -0600

    Set up problem 2

--- Problem2.py
+++ Problem2.py
@@ -1,1 +1,10 @@
 # Writen By: Jeremy Fischer, , and
+
+from Problem1 import TwoStack
+
+class Queue():
+    def __init__(self, size):
+        myStack = TwoStack(size/2, size/2)
+
+    def Enqueue(self):
+        
