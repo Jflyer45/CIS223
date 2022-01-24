9974f49126e7971a9c5e303d5d086957237c46b3
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-01-24 13:19:34 -0600

    Add more tests

--- Driver.py
+++ Driver.py
@@ -2,7 +2,7 @@
 from Problem1 import TwoStack
 from Problem2 import Queue
 
-# Queue Testing
+#Queue Testing
 myQueue = Queue(8)
 
 myQueue.Enqueue(1)
@@ -17,41 +17,56 @@ print(myQueue.Dequeue())
 print(myQueue.Dequeue())
 print(myQueue.Dequeue())
 print(myQueue.Dequeue())
-myQueue.Enqueue(1)
-myQueue.Enqueue(2)
-myQueue.Enqueue(3)
-myQueue.Dequeue()
-myQueue.Dequeue()
-
+# myQueue.Enqueue(1)
+# myQueue.Enqueue(2)
+# myQueue.Enqueue(3)
+# myQueue.Dequeue()
+# myQueue.Dequeue()
+#
 print("Head: " + str(myQueue.head))
 print("Tail: " + str(myQueue.tail))
 print(myQueue.myStack.array)
+#
+# # Smaller Queue Testing
+# otherQu = Queue(2)
+# otherQu.Enqueue(1)
+# otherQu.Enqueue(2)
+# otherQu.Dequeue()
+#
+# print(otherQu.myStack.array)
+#
+# # TwoStack Testing
+# testCase = TwoStack(2, 3)
+#
+# testCase.Push1(1)
+# testCase.Push1(2)
+#
+# testCase.Push2(3)
+# testCase.Push2(4)
+# testCase.Push2(5)
+# testCase.Push2(466)
+# testCase.Push2(13241234)
+# print(testCase.head2)
+#
+# print(testCase.array)
+#
+# print(testCase.isFull1())
+# print(testCase.isFull2())
 
-# Smaller Queue Testing
-otherQu = Queue(2)
-otherQu.Enqueue(1)
-otherQu.Enqueue(2)
-otherQu.Dequeue()
 
-print(otherQu.myStack.array)
-
-# TwoStack Testing
-testCase = TwoStack(2, 3)
-
-testCase.Push1(1)
-testCase.Push1(2)
-
-testCase.Push2(3)
-testCase.Push2(4)
-testCase.Push2(5)
-testCase.Push2(466)
-testCase.Push2(13241234)
-print(testCase.head2)
-
-print(testCase.array)
-
-print(testCase.isFull1())
-print(testCase.isFull2())
+# Testing for NONE
+# myQueue = Queue(4)
+# myQueue.Enqueue(None)
+# myQueue.Enqueue(None)
+# myQueue.Enqueue(None)
+# myQueue.Enqueue(3)
+# print(myQueue.myStack.array)
+#
+# myQueue.Dequeue()
+# myQueue.Dequeue()
+# myQueue.Dequeue()
+# print(myQueue.Dequeue())
+# print(myQueue.myStack.array)
 
 
 
