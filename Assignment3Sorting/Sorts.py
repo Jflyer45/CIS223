3c92fc247df930725741b7e4e22a647990ac4454
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-02-21 15:12:58 -0600

    Added comments to my code

--- Assignment3Sorting/Sorts.py
+++ Assignment3Sorting/Sorts.py
@@ -1,20 +1,21 @@
 # Written by Jeremy Fischer, Nate Bursch, Sungjae Oh
 
-# Problem 1
+# Problem 1 Jeremy Fischer
+# Code is based off class code from slides
 def insertionSort(array, isAscendingOrder):
     for i in range(1, len(array)):
         currentNumber = array[i]
         j = i - 1
-
+        
+        # If ascending currentNumber < array[j]
         if isAscendingOrder:
             while j >= 0 and currentNumber < array[j]:
-                # Swap
                 array[j + 1] = array[j]
                 j -= 1
             array[j + 1] = currentNumber
+        # Else sort by descending by currentNumber > array[j]
         else:
             while j >= 0 and currentNumber > array[j]:
-                # Swap
                 array[j + 1] = array[j]
                 j -= 1
             array[j + 1] = currentNumber
