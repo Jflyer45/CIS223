7cb30f4d412a05d6b013149e93adccffcc939895
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-02-21 13:25:30 -0600

    Added helper functions

--- Assignment3Sorting/Timing.py
+++ Assignment3Sorting/Timing.py
@@ -6,6 +6,15 @@ def getUnsortedArray(size):
         array.append(random.randint(0, 1000))
     return array
 
+def checkIfArrayIsSortedAscending(array):
+
+    for i in range(len(array) - 1):
+        if array[i] < array[i+1] or i == array[i+1]:
+            continue
+        else:
+            return False
+    return True
+
 t = time.process_time()
 sortedArray = None # put sort here
 elapsed_time = time.process_time() - t
