56e6efec90e15cd2ed129e3bcf818eb58a328161
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-02-21 13:34:19 -0600

    Fixed helper code, import sorting code

--- Assignment3Sorting/Timing.py
+++ Assignment3Sorting/Timing.py
@@ -1,4 +1,6 @@
 import time, random
+from Assignment3Sorting.Sorts import insertionSort
+
 
 def getUnsortedArray(size):
     array = []
@@ -9,12 +11,17 @@ def getUnsortedArray(size):
 def checkIfArrayIsSortedAscending(array):
 
     for i in range(len(array) - 1):
-        if array[i] < array[i+1] or i == array[i+1]:
+        if array[i] < array[i+1] or array[i] == array[i+1]:
             continue
         else:
             return False
     return True
 
+test = getUnsortedArray(10_000)
 t = time.process_time()
-sortedArray = None # put sort here
+sortedArray = insertionSort(test) # put sort here
 elapsed_time = time.process_time() - t
+
+print(elapsed_time)
+print(checkIfArrayIsSortedAscending(test))
+print(test)
