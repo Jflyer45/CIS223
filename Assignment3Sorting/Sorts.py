ad48090009c553d403d1e91369f08e9bc10ad0ee
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-02-27 20:19:25 -0600

    Revert "Testing Function Update"
    This reverts commit 9cc632fede641fe4ec7bbd81086ba71abf075404.

--- Assignment3Sorting/Sorts.py
+++ Assignment3Sorting/Sorts.py
@@ -2,25 +2,17 @@
 import time, random
 # Problem 1 Jeremy Fischer
 # Code is based off class code from slides
-def insertionSort(array, isAscendingOrder):
+def insertionSort(array):
     for i in range(1, len(array)):
         currentNumber = array[i]
         j = i - 1
 
-        # If ascending currentNumber < array[j]
-        if isAscendingOrder:
-            # Goes through all the numbers backwards until it finds it's correct spot
-            while j >= 0 and currentNumber < array[j]:
-                array[j + 1] = array[j]
-                j -= 1
-            # Places it at it's correct spot
-            array[j + 1] = currentNumber
-        # Else sort by descending by currentNumber > array[j]
-        else:
-            while j >= 0 and currentNumber > array[j]:
-                array[j + 1] = array[j]
-                j -= 1
-            array[j + 1] = currentNumber
+        # Goes through all the numbers backwards until it finds it's correct spot
+        while j >= 0 and currentNumber < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        # Places it at it's correct spot
+        array[j + 1] = currentNumber
 
 
 # Problem 2
@@ -29,7 +21,7 @@ def MergeSort(A):
 
     if len(A) <= 1:
         #print("Returning original Array ............")
-        #print(A)
+        print(A)
         return A
 
     else:
@@ -138,12 +130,58 @@ def countingSort(arraytoCount,isAcending):
 
 
 
+
+def getUnsortedArray(size):
+    array = []
+    for i in range(size):
+        array.append(random.randint(1, 1000000))
+    return array
+
+def checkIfArrayIsSortedAscending(array):
+    for i in range(len(array) - 1):
+        if array[i] < array[i+1] or array[i] == array[i+1]:
+            continue
+        else:
+            return False
+    return True
+
+TotalTime = 0
+size = 1_000_000
+for i in range(3):
+    
+    test = getUnsortedArray(size)
+    Acending = True
+    print("Test {}".format(i))
+    t = time.process_time()
+    sortedArray = countingSort(test,Acending)
+    elapsed_time = time.process_time() - t
+    print(checkIfArrayIsSortedAscending(sortedArray))
+    #print(sortedArray)
+    print("Time is: {}".format(elapsed_time))
+    TotalTime += elapsed_time
+    
+
+for j in range(3):
+    
+    test = getUnsortedArray(size)
+    Acending = False
+    print("Test {}".format(j))
+    t = time.process_time()
+    sortedArray = countingSort(test,Acending)
+    elapsed_time = time.process_time() - t
+    print(checkIfArrayIsSortedAscending(sortedArray))
+    #print(sortedArray)
+    print("Time is: {}".format(elapsed_time))
+    TotalTime += elapsed_time
+
+print("Average Time = {}".format(TotalTime/6))
+
 # def getUnsortedArray(size):
 #     array = []
 #     for i in range(size):
-#         array.append(random.randint(1, 1000000))
+#         array.append(random.randint(1, 10000))
 #     return array
-
+#
 # def checkIfArrayIsSortedAscending(array):
 #     for i in range(len(array) - 1):
 #         if array[i] < array[i+1] or array[i] == array[i+1]:
@@ -151,34 +189,16 @@ def countingSort(arraytoCount,isAcending):
 #         else:
 #             return False
 #     return True
+#
+# size = 10000
+# test = getUnsortedArray(size)
+#
+# t = time.process_time()
+# sortedArray = countingSort(test)
+# elapsed_time = time.process_time() - t
+#
+# print(checkIfArrayIsSortedAscending(sortedArray))
+# #print(sortedArray)
+# print("Time is: ")
+# print(elapsed_time)
 
-# TotalTime = 0
-# size = 1_000_000
-# for i in range(3):
-    
-#     test = getUnsortedArray(size)
-#     Acending = True
-#     print("Test {}".format(i))
-#     t = time.process_time()
-#     sortedArray = countingSort(test,Acending)
-#     elapsed_time = time.process_time() - t
-#     print(checkIfArrayIsSortedAscending(sortedArray))
-#     #print(sortedArray)
-#     print("Time is: {}".format(elapsed_time))
-#     TotalTime += elapsed_time
-    
-
-# for j in range(3):
-    
-#     test = getUnsortedArray(size)
-#     Acending = False
-#     print("Test {}".format(j))
-#     t = time.process_time()
-#     sortedArray = countingSort(test,Acending)
-#     elapsed_time = time.process_time() - t
-#     print(checkIfArrayIsSortedAscending(sortedArray))
-#     #print(sortedArray)
-#     print("Time is: {}".format(elapsed_time))
-#     TotalTime += elapsed_time
-
-# print("Average Time = {}".format(TotalTime/6))
