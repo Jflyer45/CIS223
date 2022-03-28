6e47656cc27bb1a1ee67ef0f64fd81d40703c9d5
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-03-28 12:13:07 -0500

    Set up linear search

---
+++ Assignment5/Main.py
@@ -0,0 +1,5 @@
+from Search import *
+
+test = [1, 3, 4, 1, 2 ,3]
+
+print(linearSearch(test, 8))
---
+++ Assignment5/Search.py
@@ -0,0 +1,8 @@
+# Task 1
+def linearSearch(A, key):
+    doesExist = False
+    for item in A:
+        if item == key:
+            doesExist = True
+            break
+    return doesExist
