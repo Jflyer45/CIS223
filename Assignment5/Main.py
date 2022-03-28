fb75fb64aa325b9ae9c7a7fe2ed53c426e34a304
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-03-28 12:44:41 -0500

    Set up testingTime to also work with binary

--- Assignment5/Main.py
+++ Assignment5/Main.py
@@ -8,16 +8,19 @@ def getAscendingArray(size):
         array.append(i+1)
     return array
 
-def testingTime(size, target):
+def testingTime(size, target, mode):
     test = getAscendingArray(size)
     times = []
     for i in range(1, 4):
         t = time.process_time()
-        print(linearSearch(test, target))
+        if mode.lower() == "l":
+            print(linearSearch(test, target))
+        else:
+            print(BinarySearch(test, target))
         elapsed_time = time.process_time() - t
         times.append(elapsed_time)
     for i in times:
         print(i)
     print("Average: " + str((times[0]+times[1]+times[2])/3))
 
-testingTime(100_000, 1)
+testingTime(100_000, -1, "l")
