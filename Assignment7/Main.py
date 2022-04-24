040dec8317557d5f3fb1e3c4906bd04df1d34469
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-04-24 11:32:21 -0500

    Set up BFS, doesn't seem to be doing the right order tho

--- Assignment7/Main.py
+++ Assignment7/Main.py
@@ -5,3 +5,6 @@ G = {"P":["Q","R"], "Q":["P","R"], "R":["P","Q"]}
 
 # Task 4
 G2 = {"A":["C", "B"], "B":["A", "D", "E"], "C":["A", "F"], "D":["B"], "E": ["B", "F"], "F": ["C", "E"]}
+
+discoveredNodes = BFS(G2, "A")
+print(discoveredNodes)
--- Assignment7/SearchFunctions.py
+++ Assignment7/SearchFunctions.py
@@ -1,5 +1,17 @@
-def BFS():
-    pass
+def BFS(graph, start):
+    frontierQueue = []
+    discovered = []
+    frontierQueue.append(start)
+    discovered.append(start)
+
+    while(len(frontierQueue) != 0):
+        current = frontierQueue.pop()
+        print(f"We have visited {current}")
+        for vertex in graph[current]:
+            if(vertex not in discovered):
+                frontierQueue.append(vertex)
+                discovered.append(vertex)
+    return discovered
 
 def DFS():
     pass
